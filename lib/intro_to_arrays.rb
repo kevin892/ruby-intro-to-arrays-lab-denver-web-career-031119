242763dc3ecc869a84bc29f22995eda313594944
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 22:39:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,6 +6,7 @@ def array_with_two_elements
   [1,2]
 end
 
-def first_element(spot)
-  spot = taylor_swift.first
+def first_element
+@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+@taylor_swift.fist
 end
