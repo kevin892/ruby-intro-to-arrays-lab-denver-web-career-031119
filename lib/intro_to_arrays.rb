4fa572007caea7b8c87b6721720bfa9f0811b90e
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 22:37:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,15 +6,4 @@ def array_with_two_elements
   [1,2]
 end
 
-def first_element
-  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  taylor_swift.first
-end
-
-def third_element
-  array[2]
-end
-
-def last_element
-  array.last
-end
+taylor_swift[0]
