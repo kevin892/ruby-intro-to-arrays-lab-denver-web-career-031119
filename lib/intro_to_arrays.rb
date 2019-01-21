a2e3024fc5012bb2caef0ea8d2402c4d882bc749
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 22:14:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,8 +2,7 @@ def instantiate_new_array
   []
 end
 
-def array_with_two_elements(first_name,last_name)
-  @first_name=first_name
-  @last_name=last_name
+def array_with_two_elements
+[first_name,last_name]
 end
-end
+
