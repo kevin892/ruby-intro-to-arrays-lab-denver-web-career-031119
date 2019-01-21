f20d724901b60a642bd5bee986ec8d9e6755d9f9
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 22:24:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,15 @@ end
 def array_with_two_elements
   [1,2]
 end
+
+def first_element
+  taylor_swift.first
+end
+
+def third_element
+  taylor_swift[2]
+end
+
+def last_element
+  taylor_swift.last
+end
